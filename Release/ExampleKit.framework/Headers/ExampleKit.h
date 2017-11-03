ee45dd177f732527b2e740c1536022ba22f57682
Varun Santhanam <varun.santhanam@sherbit.io>
2017-11-03 11:46:32 -0500

    added release

---
+++ Release/ExampleKit.framework/Headers/ExampleKit.h
@@ -0,0 +1,20 @@
+//
+//  ExampleKit.h
+//  ExampleKit
+//
+//  Created by Varun Santhanam on 10/26/17.
+//  Copyright © 2017 Varun Santhanam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ExampleKit.
+FOUNDATION_EXPORT double ExampleKitVersionNumber;
+
+//! Project version string for ExampleKit.
+FOUNDATION_EXPORT const unsigned char ExampleKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ExampleKit/PublicHeader.h>
+
+#import <ExampleKit/EKObject.h>
+#import <ExampleKit/EKSession.h>
